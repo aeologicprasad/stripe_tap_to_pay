07ef966b7e04bf99674ad5a5f28ff2c96444c2a8
Prasad Rawas <prasad@aeologic.com>
2023-08-29 12:43:04 +0530

    reader model modified

--- lib/data/reader.dart
+++ lib/data/reader.dart
@@ -1,51 +1,46 @@
-import 'device_type.dart';
-import 'location.dart';
-import 'location_status.dart';
-import 'network_status.dart';
-
 class Reader {
-  final DeviceType deviceType;
-  final LocationStatus locationStatus;
+  // final DeviceType deviceType;
+  // final LocationStatus locationStatus;
   final String? id;
   final bool isSimulated;
-  final NetworkStatus? networkStatus;
+  // final NetworkStatus? networkStatus;
   final String? rawSerialNumber;
   final String? label;
   final String? deviceSwVersion;
   final String? baseUrl;
   final String? ipAddress;
   final bool? livemode;
-  final Location? location;
+  // final Location? location;
 
   Reader({
-    required this.deviceType,
-    required this.locationStatus,
+    // required this.deviceType,
+    // required this.locationStatus,
     this.id,
     required this.isSimulated,
-    this.networkStatus,
+    // this.networkStatus,
     this.rawSerialNumber,
     this.label,
     this.deviceSwVersion,
     this.baseUrl,
     this.ipAddress,
     this.livemode,
-    this.location,
+    // this.location,
   });
 
   factory Reader.fromJson(Map<String, dynamic> json) {
     return Reader(
-      deviceType: json['deviceType'],
-      locationStatus: json['locationStatus'],
+      // deviceType: json['deviceType'],
+      // locationStatus: json['locationStatus'],
       id: json['id'],
       isSimulated: json['isSimulated'],
-      networkStatus: json['networkStatus'],
+      // networkStatus: json['networkStatus'],
       rawSerialNumber: json['rawSerialNumber'],
       label: json['label'],
       deviceSwVersion: json['deviceSwVersion'],
       baseUrl: json['baseUrl'],
       ipAddress: json['ipAddress'],
       livemode: json['livemode'],
-      location: json['location'],
+      // location: json['location'],
     );
   }
 }
